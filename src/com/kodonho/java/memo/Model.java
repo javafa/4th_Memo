eeaf61e36ee4f21d97f269ee34158e3b61fe731d
javafa <nigne2016@gmail.com>
2017-09-08 12:43:55 +0900

    add list

--- src/com/kodonho/java/memo/Model.java
+++ src/com/kodonho/java/memo/Model.java
@@ -1,10 +1,13 @@
 package com.kodonho.java.memo;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
@@ -48,19 +51,16 @@ public class Model {
 	public void create(Memo memo){
 		//글번호
 		memo.no = ++lastIndex;
-		// memo 객체의 내용을 파일에 쓴다
-		System.out.println(database.getPath());
+
+		// 1. 쓰는 스트림을 연다
 		try(FileOutputStream fos = new FileOutputStream(database, true)) {
 			// 저장할 내용을 구분자로 분리하여 한줄의 문자열로 바꾼다.
 			String row = memo.no + COLUMN_SEP 
 					+ memo.name + COLUMN_SEP 
 					+ memo.content + COLUMN_SEP 
 					+ memo.datetime + "\n";
-			System.out.println("한줄|"+row);
-			// 1. 쓰는 스트림을 연다
-			
 			// 2. 스트림을 중간처리... (텍스트의 엔코딩을 변경...)
-			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			OutputStreamWriter osw = new OutputStreamWriter(fos); // 래퍼스트림
 			// 3. 버퍼처리...
 			BufferedWriter bw = new BufferedWriter(osw);
 			bw.append(row);
@@ -71,8 +71,7 @@ public class Model {
 		}
 		
 		// 글 하나를 저장한 메모리를 저장소로 이동
-		list.add(memo);
-		
+		// list.add(memo);
 	}
 	// 읽기
 	public Memo read(int no){
@@ -97,6 +96,52 @@ public class Model {
 	}
 	// 목록
 	public ArrayList<Memo> getList(){
+		
+		// 데이터가 중복해서 쌓이지 않도록 저장소를 지워주는 작업이 필요한 경우가 있다.
+		list.clear();
+		
+		// 1. 읽는 스트림을 연다
+		try(FileInputStream fis = new FileInputStream(database)){ // try-with 절에서 자동으로 fis.close가 발생
+			// 2. 실제 파일 엔코딩을 바꿔주는 래퍼 클래스를 사용
+			InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+			// 3. 버퍼처리
+			BufferedReader br = new BufferedReader(isr);
+			
+			String row;
+			// 새로운 줄을 한줄씩 읽어서 row에 저장하고
+			// 더 이상 읽을 데이터가 없으면 null이 리턴되므로 로직이 종료된다.
+			while( (row = br.readLine()) != null ){
+				String tempRow[] = row.split(COLUMN_SEP);
+				// 1::fds::fdsaf::1504839497021
+				// tempRow[0] = 1
+				// tempRow[1] = fds 
+				// tempRow[2] = fdsaf
+				// tempRow[3] = 1504839497021
+				Memo memo = new Memo();
+				memo.no = Integer.parseInt(tempRow[0]);
+				memo.name = tempRow[1];
+				memo.content = tempRow[2];
+				memo.datetime = Long.parseLong(tempRow[3]);
+				
+				list.add(memo);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return list;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
